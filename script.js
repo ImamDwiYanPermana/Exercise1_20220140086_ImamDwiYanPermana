5606b4c4567378bdcdb94d502cd2df3fd9d59da3
susi <yuanliu755@gmail.com>
2024-03-28 01:13:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAlert() {
+  alert(Date(), tampil(), show());
+  Date();
+
+  let tanggal = new date();
+  documentge.getElementByid("hasil").innerHTML = tanggal;
+
+  function tampil() {
+    alert(
+      "istilah edelwis berasal dari bahasa jerman yang berati mulia dan berwarna"
+    );
+  }
+
+  function show() {
+    alert("warna putih edelwis melambangkan era kebangsaan ");
+  }
+}
